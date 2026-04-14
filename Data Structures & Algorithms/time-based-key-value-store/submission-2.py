7d4fe8c96d859b54b28cf7b8e6944ae9ee887ecf
Daniel Luu <64342554+DanielLuu122@users.noreply.github.com>
2026-04-14 18:06:39 -0400

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,16 @@
+import bisect
+class TimeMap:
+
+    def __init__(self):
+        self.m = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.m[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        arr = self.m[key]
+        index = bisect.bisect_right(arr, timestamp, key=lambda v:v[0]) - 1
+        print(index, arr, timestamp)
+        if index > len(arr) or index < 0:
+            return ""
+        return arr[index][1]
